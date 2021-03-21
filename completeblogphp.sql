c1464ab1ec43baac9bd0b8344f2be38a76b99911
WarrenGSlice <77131351+WarrenGSlice@users.noreply.github.com>
2021-03-20 18:23:16 -0700

    Add files via upload

---
+++ completeblogphp.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 20, 2021 at 11:39 PM
+-- Server version: 5.7.24
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `completeblogphp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL,
+  `views` int(11) NOT NULL DEFAULT '0',
+  `image` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `published` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `user_id`, `title`, `slug`, `views`, `image`, `body`, `published`, `created_at`, `updated_at`) VALUES
+(1, 1, '5 Habits that can improve your life', '5-habits-that-can-improve-your-life', 0, 'banner.jpg', 'Read every day', 1, '2018-02-03 15:58:02', '2018-02-02 03:14:31'),
+(2, 1, 'Second post on LifeBlog', 'second-post-on-lifeblog', 0, 'banner.jpg', 'This is the body of the second post on this site', 1, '2021-03-01 01:02:17', '2018-02-01 21:04:36'),
+(3, 2, 'I Lied, I Cheated, I Succeeded, The story of a young man who turned his life around and created a financial empire', 'i-lied-i-cheated-i-succeeded-the-story-of-a-young-man-who-turned-his-life-around-and-created-a-financial-empire', 0, 'serene.jpg', '<p>Growing up in the shadow of the I-5 corridor was not an easy place to escape temptation as a young man. Many youths found that when life became too hard to deal with, there were many ways to escape. Incorrectly dealing with problems as a young person happens almost as frequently as sneezing. My life as a teenager went down the rails as happens to so many others. Turning to drugs to find some kind of semblance of peace quickly turned into 10 years of out of control drug abuse and criminal behavior that landed me in jail over and over and over again. In fact, 14 times I was&nbsp;incarcerated for various crimes in many states and many types of facilities. It appeared that life was not going to end well for this young man. Of course being imprisoned can have its benefits and it gives a young person time to reflect if they choose to seek that opportunity. I was not one of those people. Instead the experience would make me more angry and each time it hardened my heart even more. So how did I managed to break this cycle and turn into a wildly successful entrepreneur? Tune in for the next blog post to see what happens.&nbsp;</p>\r\n', 1, '2021-03-01 02:16:17', '2021-03-01 00:49:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_topic`
+--
+
+CREATE TABLE `post_topic` (
+  `id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `topic_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `post_topic`
+--
+
+INSERT INTO `post_topic` (`id`, `post_id`, `topic_id`) VALUES
+(1, 1, 1),
+(2, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topics`
+--
+
+CREATE TABLE `topics` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `topics`
+--
+
+INSERT INTO `topics` (`id`, `name`, `slug`) VALUES
+(3, 'Diary', 'diary'),
+(1, 'Inspiration', 'inspiration'),
+(2, 'Motivation', 'motivation');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` varchar(25) NOT NULL,
+  `zip` varchar(10) NOT NULL,
+  `country` varchar(25) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` enum('Author','Admin') DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `firstname`, `lastname`, `address`, `city`, `state`, `zip`, `country`, `email`, `role`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'Awa', '', '', '', '', '', '', '', 'info@codewithawa.com', 'Admin', 'mypassword', '2018-01-08 20:52:58', '2018-01-08 20:52:58'),
+(2, 'Warrengslice', '', '', '', '', '', '', '', 'WarrenPeterson141414@gmail.com', 'Admin', '5f4dcc3b5aa765d61d8327deb882cf99', '2021-02-28 09:33:57', '2021-02-28 09:33:57'),
+(3, 'Admin', '', '', '', '', '', '', '', 'Admin@Admin.com', 'Admin', '5f4dcc3b5aa765d61d8327deb882cf99', '2021-02-28 10:09:04', '2021-02-28 10:09:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `post_topic`
+--
+ALTER TABLE `post_topic`
+  ADD UNIQUE KEY `post_id` (`post_id`),
+  ADD KEY `post_topic_ibfk_1` (`topic_id`);
+
+--
+-- Indexes for table `topics`
+--
+ALTER TABLE `topics`
+  ADD UNIQUE KEY `slug` (`slug`) USING BTREE;
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `post_topic`
+--
+ALTER TABLE `post_topic`
+  ADD CONSTRAINT `Constraint name` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  ADD CONSTRAINT `post_topic_ibfk_1` FOREIGN KEY (`topic_id`) REFERENCES `post_topic` (`post_id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
